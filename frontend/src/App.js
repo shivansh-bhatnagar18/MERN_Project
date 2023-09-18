4031de227c03edbd0fb6ccdd422e1ba62061a7ac
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2023-09-18 13:47:44 +0000

    created a react application

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from  'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import './App.css';
+import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path = "/" exact><Users /></Route>
+        <Route path = "/places/new"><NewPlace /></Route>
+        <Redirect to="/" /> 
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
