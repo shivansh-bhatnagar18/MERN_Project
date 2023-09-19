3c10b8f49fd5ac5fc41e81a8ff10a61c41db7167
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2023-09-19 09:13:46 +0000

    Made a basic React homepage with users list

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { useState } from 'react';
 
 import MainHeader from './MainHeader';
-import NavLinks from './NavLinks'
-import SideDrawer from './SideDrawer'
+import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 import "./MainNavigation.css"
+import Backdrop from '../UIElements/Backdrop';
 
 const  MainNavigation = props => {
+
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    }
+
     return (
             <React.Fragment>
-                <SideDrawer>
-                    <nav className = 'main-navigation__drawer-nav'>
+                {drawerIsOpen && <Backdrop onClick = {closeDrawerHandler}/>}
+                <SideDrawer show = {drawerIsOpen} onClick={closeDrawerHandler}>
+                    <nav className='main-navigation__drawer-nav'>
                         <NavLinks />
                     </nav>
                 </SideDrawer>
                 <MainHeader>
-                    <button className='main-navigation__menu-btn'>
+                    <button className='main-navigation__menu-btn' onClick = {openDrawerHandler}>
                         <span />
                         <span />
                         <span />
