14109c0138c647cf643f56f885e47800dd8b1a13
Shivansh Bhatnagar <shivansh.bhatnagar.mat22@itbhu.ac.in>
2023-09-20 10:31:08 +0000

    added valid input

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import './NewPlace.css'
+import Input from '../../shared/components/FormElements/Input';
+import {VALIDATOR_REQUIRE} from '../../shared/utils/validators'
+
 const NewPlace = () => {
-    return <h2>New Place Works</h2>
+
+    const changeHandler = event => {
+        
+    }
+
+    return <form className='place-form'>
+        <Input element = 'input' 
+        type = 'text' 
+        label = 'Title' 
+        validators = {[VALIDATOR_REQUIRE()]} 
+        errorText = 'Please enter a valid text'/>
+    </form>
 }
 
 export default NewPlace;
